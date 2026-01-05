5b72635b101e7d6c2fce33d2e67baf02b30b68de
Pham Huy Hoang <huyhoang121795@gmail.com>
2026-01-05 09:57:45 +0700

    update style

--- src/components/FixedButtons/index.tsx
+++ src/components/FixedButtons/index.tsx
@@ -34,8 +34,8 @@ const FixedButtons: React.FC = () => {
         ></div>
         <a
           className={clsx(
-            "z-[1] lg:px-7 lg:py-1 p-2 bg-gradient-to-b from-[#FFFFFF] to-[#F6E7EE] !text-[#D5AF34] rounded-2xl text-center font-prata lg:rotate-[-2deg] border border-[#9E7B6A]",
-            responsiveFontSizeArray(24, 47)
+            "z-[1] lg:px-7 lg:py-1 p-2 bg-gradient-to-b from-[#FFFFFF] to-[#F6E7EE] !text-[#D5AF34] rounded-2xl text-center font-prata lg:rotate-[-2deg] border border-[#9E7B6A] min-[375px]:text-[24px]",
+            responsiveFontSizeArray(20, 47)
           )}
           title="BOOKING"
           href="https://booking.spacepos.net/?id=jzOR8l!BpuM="
@@ -53,8 +53,8 @@ const FixedButtons: React.FC = () => {
         </a>
         <a
           className={clsx(
-            "text-[#9E7B6A] z-[1] hover:text-[#9E7B6A]/80 cursor-pointer",
-            responsiveFontSizeArray(24, 60)
+            "text-[#9E7B6A] z-[1] hover:text-[#9E7B6A]/80 cursor-pointer min-[375px]:text-[24px]",
+            responsiveFontSizeArray(20, 60)
           )}
           onClick={handlePhoneClick}
           style={{
