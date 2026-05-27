08759166e3364c08268326afc958424cfbdfb138
hoangpham <huyhoang121795@gmail.com>
2026-05-27 12:46:07 +0700

    update code

--- src/pages/OurPolicies/components/PolicyFooter/index.tsx
+++ src/pages/OurPolicies/components/PolicyFooter/index.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import clsx from "clsx";
 import { responsiveFontSizeArray } from "@/shared/utils/helper";
 import ourPoliciesContent from "@/content/ourPolicies.json";
+import { OrnamentalDivider } from "@/components/OrnamentalDivider";
 
 export const PolicyFooter: React.FC = () => {
-  const footer = (ourPoliciesContent as { footer: { thankYou: string; contactPrompt: string } }).footer;
+  const footer = (
+    ourPoliciesContent as {
+      footer: { thankYou: string; contactPrompt: string };
+    }
+  ).footer;
   return (
     <footer className="text-center pt-6 lg:pt-10">
-      <div
-        className="inline-block h-px w-16 mb-6 bg-gradient-to-r from-transparent via-madison-gold/60 to-transparent"
-        aria-hidden
-      />
+      <OrnamentalDivider className="mx-auto mb-6 text-madison-gold" />
       <p
         className={clsx(
           "font-extralight text-madison-gold-dark italic",
